d9a4fd914d56562cbc29c664d4cb5a1881bf6572
DESKTOP-L9JNC77\lisowolf <minebuilder445@gmail.com>
2021-11-21 23:03:17 +0600

    Added localization (russian language)

--- LisoTetris/Program.cs
+++ LisoTetris/Program.cs
@@ -2,6 +2,7 @@ using LisoTetris;
 using LisoTetris.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Net.Http;
 
@@ -9,6 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddScoped<IStringLocalizer<App>, StringLocalizer<App>>();
 builder.Services.AddSingleton<TetrisStateContainer>();
 
 await builder.Build().RunAsync();
