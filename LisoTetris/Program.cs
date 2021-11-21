9b63be4c281372b37d17bb459d3cda9c5e939002
DESKTOP-L9JNC77\lisowolf <minebuilder445@gmail.com>
2021-11-21 18:56:47 +0600

    Moved tetris state to service

--- LisoTetris/Program.cs
+++ LisoTetris/Program.cs
@@ -1,11 +1,14 @@
+using LisoTetris;
+using LisoTetris.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<LisoTetris.App>("#app");
+builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<TetrisState>();
 
 await builder.Build().RunAsync();
